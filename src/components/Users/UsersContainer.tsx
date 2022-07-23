a3e851985fce96fe15bdf430e24227ef20af078c
stacy <ada.davis27@gmail.com>
2022-07-23 18:52:09 +0300

    web socket

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,7 +1,7 @@
+import {getIsFetching} from '../../redux/users-selectors';
+import {Preloader} from '../common/Preloader/Preloader';
 import {useAppSelector} from '../../redux/redux-store';
 import {Users} from './Users';
-import {Preloader} from '../common/Preloader/Preloader';
-import {getIsFetching} from '../../redux/users-selectors';
 
 export const UsersPage = () => {
     const isFetching = useAppSelector(getIsFetching)
