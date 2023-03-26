0c2e924c74d5175b2477ef375d1e27abdd99282c
stacy <ada.davis27@gmail.com>
2023-03-26 23:06:31 +0300

    absolute imports

--- src/hoc/withSuspense.tsx
+++ src/hoc/withSuspense.tsx
@@ -1,5 +1,5 @@
 import React, {ComponentType} from 'react';
-import {Preloader} from '../components/common/Preloader/Preloader';
+import {Preloader} from 'components/common/Preloader/Preloader';
 
 export function withSuspense<T>(Component: ComponentType<T>) {
     return (props: T) => <React.Suspense fallback={<Preloader/>}>
