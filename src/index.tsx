d46e7497a402d598def6994c0e4307e54ebb95f2
stacy <ada.davis27@gmail.com>
2024-01-27 10:47:11 +0300

    refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,17 @@
-import ReactDOM from 'react-dom'
+import {render} from 'react-dom'
 import reportWebVitals from './reportWebVitals'
 import './index.css'
-import App from "./App"
+import App from './App'
 import {HashRouter} from 'react-router-dom'
-import {store} from "./redux/redux-store"
-import {Provider} from "react-redux"
+import {store} from 'redux/redux-store'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(
+render(
     <HashRouter>
         <Provider store={store}>
-            <App />
+            <App/>
         </Provider>
     </HashRouter>,
     document.getElementById('root'))
 
 reportWebVitals()
-
-// const rootElement = document.getElementById('root');
-// if (!rootElement) throw new Error('Failed to find the root element');
-// const root = createRoot(rootElement);
