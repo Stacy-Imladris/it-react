814a0af04504dc8155fb9298f780684fc8f9d037
stacy <ada.davis27@gmail.com>
2024-02-21 14:20:41 +0300

    refactoring

--- src/hoc/withSuspense.tsx
+++ src/hoc/withSuspense.tsx
@@ -1,8 +1,8 @@
-import React, {ComponentType} from 'react';
+import {ComponentType, Suspense} from 'react';
 import {Preloader} from 'components/common/Preloader/Preloader';
 
 export function withSuspense<T>(Component: ComponentType<T>) {
-    return (props: T) => <React.Suspense fallback={<Preloader/>}>
+    return (props: T) => <Suspense fallback={<Preloader/>}>
         <Component {...props}/>
-    </React.Suspense>
+    </Suspense>
 }
