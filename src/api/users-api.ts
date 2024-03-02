a3b26b39837f46fd1ec819f9bd40ccef43f9a44d
stacy <ada.davis27@gmail.com>
2024-03-02 11:01:19 +0300

    refactoring

--- src/api/users-api.ts
+++ src/api/users-api.ts
@@ -3,11 +3,12 @@ import {instance} from './api';
 import {FilterType} from 'redux/users-reducer';
 
 export const usersAPI = {
-    getUsers(currentPage: number = 1, pageSize: number = 10, filter: FilterType = {
-        term: '',
-        friend: null
-    }) {
-        return instance.get<any, AxiosResponse<GetUsersResponseType>>(`users?page=${currentPage}&count=${pageSize}${filter.term ? `&term=${filter.term}` : ''}${filter.friend ? `&friend=${filter.friend}` : ''}`).then(response => response.data)
+    getUsers(currentPage: number = 1,
+             pageSize: number = 10,
+             filter: FilterType = {term: '', friend: null}) {
+        return instance.get<any, AxiosResponse<GetUsersResponseType>>(
+            `users?page=${currentPage}&count=${pageSize}${filter.term ? `&term=${filter.term}` : ''}${filter.friend ? `&friend=${filter.friend}` : ''}`)
+            .then(response => response.data)
     },
 }
 
