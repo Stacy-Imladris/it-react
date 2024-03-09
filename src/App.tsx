e658dc1ee6a0a5710ace2469c620d6eb90a0d404
stacy <ada.davis27@gmail.com>
2024-03-09 19:52:31 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -56,11 +56,9 @@ class App extends Component<AppPropsType> {
                     <Layout className="site-layout-background"
                             style={{padding: '24px 0'}}>
                         <Sider className="site-layout-background" width={200}>
-                            <Menu
-                                mode="inline"
-                                defaultSelectedKeys={['1']}
-                                style={{height: '100%'}}
-                            >
+                            <Menu mode="inline"
+                                  defaultSelectedKeys={['1']}
+                                  style={{height: '100%'}}>
                                 {
                                     [
                                         {
