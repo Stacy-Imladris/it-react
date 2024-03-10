6c6370f23f9fd32a9cd520dd899cc03377fac047
stacy <ada.davis27@gmail.com>
2024-03-10 23:34:40 +0300

    refactoring

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -7,9 +7,7 @@ const FormControl: FC<WrappedFieldProps & { placeholder?: string }> = ({input, m
 
     return (
         <div className={hasError ? s.error : ''}>
-            <div>
-                {children}
-            </div>
+            <div>{children}</div>
             {hasError && <span>{error}</span>}
         </div>
     )
@@ -19,6 +17,7 @@ export const Textarea: FC<WrappedFieldProps & { placeholder?: string }> = (props
     const {input, meta, children, ...restProps} = props
     return <FormControl {...props}><textarea {...input} {...restProps}/></FormControl>
 }
+
 export const Input: FC<WrappedFieldProps & { placeholder?: string }> = (props) => {
     const {input, meta, children, ...restProps} = props
     return <FormControl {...props}><input {...input} {...restProps}/></FormControl>
