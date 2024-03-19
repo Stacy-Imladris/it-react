6aab72e5d9d7122aa2ef760a7738b0d2c97cc1bc
stacy <ada.davis27@gmail.com>
2024-03-19 14:20:41 +0300

    refactoring

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,34 +10,33 @@ import {logout} from 'redux/auth-reducer';
 const {Header} = Layout;
 
 export const AppHeader = () => {
-    const isAuth = useAppSelector(selectIsAuth)
-    const login = useAppSelector(selectCurrentUserLogin)
+  const isAuth = useAppSelector(selectIsAuth)
+  const login = useAppSelector(selectCurrentUserLogin)
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const logoutCallback = () => {
-        dispatch(logout())
-    }
+  const logoutCallback = () => dispatch(logout())
 
-    return <Header className="header">
+  return (
+      <Header className="header">
         <Row>
-            <Col span={21}>
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                    <Menu.Item key={'1'}>
-                        <Link to={PATH.DEVELOPERS}>Developers</Link>
-                    </Menu.Item>
-                </Menu>
+          <Col span={21}>
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+              <Menu.Item key={'1'}>
+                <Link to={PATH.DEVELOPERS}>Developers</Link>
+              </Menu.Item>
+            </Menu>
+          </Col>
+          {isAuth ? <>
+            <Col span={1}>
+              <Avatar alt={login || ''}
+                      style={{backgroundColor: '#87d068'}}
+                      icon={<UserOutlined/>}/>
             </Col>
-            {
-                isAuth
-                    ? <><Col span={1}>
-                        <Avatar alt={login || ''} style={{backgroundColor: '#87d068'}}
-                                icon={<UserOutlined/>}/></Col>
-                        <Col span={1}><span style={{color: 'white'}}>{login}</span></Col>
-                        <Col span={1}><Button onClick={logoutCallback}>Log
-                            out</Button></Col></>
-                    : <Col span={3}><Button><Link to={PATH.LOGIN}>Login</Link></Button></Col>
-            }
+            <Col span={1}><span style={{color: 'white'}}>{login}</span></Col>
+            <Col span={1}><Button onClick={logoutCallback}>Log out</Button></Col>
+          </> : <Col span={3}><Button><Link to={PATH.LOGIN}>Login</Link></Button></Col>}
         </Row>
-    </Header>
+      </Header>
+  )
 }
