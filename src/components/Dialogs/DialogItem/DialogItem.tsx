fbb05b269cceaa7e7e6dd7ff39400edc6fac4c01
stacy <ada.davis27@gmail.com>
2024-03-21 00:36:40 +0300

    refactoring

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -1,13 +1,13 @@
 import s from './DialogItem.module.scss'
 import {NavLink} from 'react-router-dom'
 
-export type DialogItemPropsType = {
+type Props = {
     name: string
     id: number
     img: string
 }
 
-export const DialogItem = ({name, id, img}: DialogItemPropsType) => (
+export const DialogItem = ({name, id, img}: Props) => (
     <div className={s.dialog}>
         <div><img src={img} alt={'user image'}/></div>
         <div><NavLink to={'/dialogs/' + id}>{name}</NavLink></div>
