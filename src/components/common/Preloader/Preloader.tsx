c86f14649c9e4de4f58ca2ada5ad083cd68cbf26
stacy <ada.davis27@gmail.com>
2024-03-20 15:18:26 +0300

    refactoring

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,3 +1,3 @@
-import preloader from "assets/images/preloader.svg";
+import preloader from "assets/images/preloader.svg"
 
 export const Preloader = () => <div><img src={preloader} alt={'preloader'}/></div>
